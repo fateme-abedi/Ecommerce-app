450d065c3b69bd446ed02b524aebb03f04a9fca7
fateme <abedifateme1378@gmail.com>
2022-09-25 23:15:01 +0330

    data added to slider

---
+++ src/components/slider/Slider-data.js
@@ -0,0 +1,22 @@
+export const sliderData = [
+    {
+      image: "https://cdn.lorem.space/images/shoes/.cache/640x480/usama-akram-g3CMh2nqj_w-unsplash.jpg",
+      heading: "Shoes Villa",
+      desc: "Up to 30% off on all onsale proucts.",
+    },
+    {
+      image: "https://cdn.lorem.space/images/fashion/.cache/640x480/leon-ell-C8Q_zR8PDlA-unsplash.jpg",
+      heading: "Women Fashion",
+      desc: "Up to 30% off on all onsale proucts.",
+    },
+    {
+      image: "https://cdn.lorem.space/images/furniture/.cache/640x480/nathan-oakley--peWKMDCyw4-unsplash.jpg",
+      heading: "Furniture",
+      desc: "Up to 30% off on all onsale proucts.",
+    },
+    {
+      image: "https://cdn.lorem.space/images/watch/.cache/640x480/filip-baotic-FF8Kqb86V38-unsplash.jpg",
+      heading: "Awesome Gadgets",
+      desc: "Up to 30% off on all onsale proucts.",
+    },
+  ];
