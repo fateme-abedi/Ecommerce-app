c794123311631876dc4dee9a45f5cf1b89bc4a08
fateme <abedifateme1378@gmail.com>
2022-09-25 23:13:06 +0330

    homepage created

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Slider from '../../components/slider/Slider'
+import Products from '../../components/products/Products'
+
+function Home(props) {
+  return (
+    <div>
+      <Slider />
+      <Products />
+    </div>
+  )
+}
+
+export default Home
