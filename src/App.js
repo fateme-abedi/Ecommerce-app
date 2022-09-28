b8b7f72de8e3b92489ed7f7153f1f76e32dc2c42
fateme <abedifateme1378@gmail.com>
2022-09-29 00:43:28 +0330

    Finished !

--- src/App.js
+++ src/App.js
@@ -4,19 +4,41 @@ import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
 import Home from './pages/home/Home'
 import Contact from './pages/contact/Contact'
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
+
+import Reset from './pages/auth/Reset'
+import Products from './components/products/Products'
+import Product from './components/product/Product'
+import Cart from './components/cart/Cart'
+import Checkout from './components/checkout/Checkout'
+import './index.css'
 
 
 function App(props) {
   return (
+    <>
     <BrowserRouter>
     <Header/>
      <Routes>
-       <Route path='/' element={<Home/>}/>
-       <Route path='/contact' element={<Contact/>}/>
+       <Route path='/' element={ <Home/>}/>
+       <Route path='/contact' element={ <Contact/>}/>
+       <Route path='/login' element={ <Login/>}/>
+       <Route path='/contact' element={ <Contact/>}/>
+       
+       <Route path='/register' element={ <Register/>}/>
+       
+       <Route path='/reset' element={ <Reset/>}/>
+       <Route path='/products' element={ <Products/>}/>
+       <Route path='/products/:id' element={ <Product/>}/>
+       <Route path='/cart' element={ <Cart/>}/>
+       <Route path='/checkout' element={ <Checkout/>}/>
+       
        
      </Routes>
     <Footer/>
     </BrowserRouter>
+    </>
   );
 }
 
